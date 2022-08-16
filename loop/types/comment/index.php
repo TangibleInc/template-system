64bb4869092ef74f9c8a823c00d63ec7ab93cdf5
eliot-akira <me@eliotakira.com>
2022-08-16 11:06:13 +0200

    Start Comment loop type

---
+++ loop/types/comment/index.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tangible\Loop;
+
+/**
+ * Comment loop
+ *
+ * @see https://developer.wordpress.org/reference/..
+ */
+
+class CommentLoop extends BaseLoop {
+
+  static $loop;
+
+  static $config = [
+    'name'       => 'comment',
+    'title'      => 'Comment',
+    'category'   => 'core',
+    'query_args' => [
+    ],
+    'fields'     => [
+      'title' => [ 'description' => 'Title' ],
+    ],
+  ];
+
+  function create_query( $query_args = [] ) {
+
+    $items = [];
+
+    return $items;
+  }
+
+  /**
+   * Field
+   *
+   * Inherited `get_field` method runs a filter for extended fields, then
+   * calls `get_item_field` as needed.
+   */
+  function get_item_field( $item, $field_name, $args = [] ) {
+
+    switch ($field_name) {
+
+      case 'all':
+        ob_start();
+        ?><pre><code><?php print_r( $item ); ?></code></pre><?php
+        return ob_get_clean();
+      break;
+
+      case 'name': return $item->name;
+    }
+  }
+};
+
+CommentLoop::$loop = $loop;
+
+$loop->register_type( CommentLoop::class );
