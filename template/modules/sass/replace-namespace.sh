fd439b5f3e5cf1c31f0a4788d5f612b2783493c1
eliot-akira <me@eliotakira.com>
2022-11-22 22:18:07 +0100

    Sass module: Add script to automatically replace namespace

---
+++ template/modules/sass/replace-namespace.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+#
+# Ensure unique namespace by replacing ScssPhp with Tangible
+#
+# This is necessary because PHP and Composer don't support different versions
+# of the same module.
+#
+
+OS="`uname`"
+
+CURRENT_FOLDER="scssphp"
+
+namespace-files() {
+
+  echo "Folder: $CURRENT_FOLDER"
+  local RESTORE_FOLDER="$CURRENT_FOLDER"
+
+  for file in *.php; do
+
+    echo "  File: $file";
+
+    if [ "$OS" == "Darwin" ]; then
+      # Options for sed - Specific to macOS
+      # @see https://stackoverflow.com/questions/5694228/sed-in-place-flag-that-works-both-on-mac-bsd-and-linux#22084103
+
+      sed -i '' -e 's/namespace\ ScssPhp\\/namespace\ Tangible\\/g' "$file"
+      sed -i '' -e 's/use\ ScssPhp\\/use\ Tangible\\/g' "$file"
+    else
+      # Assume Linux (or WSL2 - Windows Subsystem for Linux)
+      sed -i -e 's/namespace\ ScssPhp\\/namespace\ Tangible\\/g' "$file"
+      sed -i -e 's/use\ ScssPhp\\/use\ Tangible\\/g' "$file"
+    fi
+  done
+
+  for folder in *; do
+    if [ -d $folder ]; then
+
+      CURRENT_FOLDER="$RESTORE_FOLDER/$folder"
+
+      cd $folder
+      namespace-files
+      cd ..
+
+    fi
+  done
+
+  CURRENT_FOLDER="$RESTORE_FOLDER"
+}
+
+cd scssphp
+
+namespace-files
+
+cd ..
