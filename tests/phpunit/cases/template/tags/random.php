784c2c7a999b1011dd1e9397221d957715edc62f
Gennady Kovshenin <gennady@kovshenin.com>
2023-03-25 20:29:28 +0300

    Tests: tags/random, tags/date

---
+++ tests/phpunit/cases/template/tags/random.php
@@ -0,0 +1,14 @@
+<?php
+class Template_Tags_Random_TestCase extends WP_UnitTestCase {
+	public function test_template_tags_random_defaults() {
+		srand(0);
+		$this->assertEquals('99', tangible_template('<Random />'), 'failed with seed 0');
+		$this->assertEquals('19', tangible_template('<Random />'), 'failed with seed 0');
+	}
+
+	public function test_template_tags_random_negative() {
+		srand(0);
+		$this->assertEquals('-100', tangible_template('<Random from=-100 to=-99/>'), 'failed with seed 0');
+		$this->assertEquals('-99', tangible_template('<Random from=-100 to=-99/>'), 'failed with seed 0');
+	}
+}
--- tests/phpunit/cases/usable.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Usable_TestCase extends WP_UnitTestCase {
-	public function test_template_system_module_is_loaded_and_usable() {
-		$this->assertTrue(function_exists('tangible_template_system'), 'tangible_template_system() is not defined');
-		$this->assertEquals('tangible_template_system', tangible_template_system()->name);
-		$this->assertNotEmpty(did_action('tangible_template_system_ready'), 'tangible_template_system is not ready');
-	}
-}
