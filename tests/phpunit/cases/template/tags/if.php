8c8afd125c9e87e046008c5318f95dfb7b6d1301
eliot-akira <me@eliotakira.com>
2023-04-07 17:43:37 +0200

    Use @wordpress/env for local dev and test environments

--- tests/phpunit/cases/template/tags/if.php
+++ tests/phpunit/cases/template/tags/if.php
@@ -6,7 +6,7 @@ class Template_Tags_If_TestCase extends WP_UnitTestCase {
 	public function test_template_tags_if_else_condition_in_html_attribute_954() {
 		$template = '<Set href_value>home</Set>';
 		$template .= '<a href="{If variable=href_value value=home}{Url home /}{Else /}{Url current /}{/If}">Click here.</a>';
-		$this->assertEquals('<a href="http://example.org">Click here.</a>', tangible_template($template));
+    $this->assertEquals('<a href="'.get_site_url().'">Click here.</a>', tangible_template($template));
 	}
 
 	/**
