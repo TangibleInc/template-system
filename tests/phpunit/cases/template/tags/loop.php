e6e5390db110f2f83b48c4eb2cb572253ce26592
eliot-akira <me@eliotakira.com>
2023-04-25 22:34:28 +0200

    Post loop type: Add attribute "sticky" for improved sticky posts support - Without sticky set, treat sticky posts as normal posts; this is the default behavior (backward compatible) - With sticky=true, put sticky posts at the top - With sticky=false, exclude sticky posts - With sticky=only, include sticky posts only

--- tests/phpunit/cases/template/tags/loop.php
+++ tests/phpunit/cases/template/tags/loop.php
@@ -1,36 +1,38 @@
 <?php
 class Template_Tags_Loop_TestCase extends WP_UnitTestCase {
-	/**
-	 * @link https://app.clickup.com/t/860qja6hm
-	 * @link https://wordpress.org/support/topic/sticky-post-not-showing-in-loop/
-	 */
-	public function test_loop_ignore_sticky_posts() {
-		[$post_id, $sticky_id, $slimey_id] = self::factory()->post->create_many(3, []);
-		stick_post($sticky_id);
+  /**
+   * Sticky posts
+   * @see /loop/types/post/index.php, attribute "sticky"
+   */
+  public function test_loop_sticky_posts() {
 
-		$this->assertEquals(
-			"[$post_id][$sticky_id][$slimey_id]",
-			tangible_template('<Loop type="post">[<Field id>]</Loop>')
-		);
+    [$post_id, $sticky_id, $post_id_2, $sticky_id_2] = self::factory()->post->create_many(4, []);
 
-		$this->assertEquals(
-			"[$post_id][$sticky_id][$slimey_id]",
-			tangible_template('<Loop type="post" sticky="default">[<Field id>]</Loop>')
-		);
+    stick_post($sticky_id);
+    stick_post($sticky_id_2);
 
-		$this->assertEquals(
-			"[$sticky_id]",
-			tangible_template('<Loop type="post" sticky="only">[<Field id>]</Loop>')
-		);
+    // Without sticky set, include sticky posts but treat them as normal posts, not at the top
+    $this->assertEquals(
+      "[$post_id][$sticky_id][$post_id_2][$sticky_id_2]",
+      tangible_template('<Loop type=post>[<Field id>]</Loop>')
+    );
 
-		$this->assertEquals(
-			"[$post_id][$slimey_id]",
-			tangible_template('<Loop type="post" sticky="hidden">[<Field id>]</Loop>')
-		);
+    // With sticky=true, include sticky posts at the top
+    $this->assertEquals(
+      "[$sticky_id][$sticky_id_2][$post_id][$post_id_2]",
+      tangible_template('<Loop type=post sticky=true>[<Field id>]</Loop>')
+    );
 
-		$this->assertEquals(
-			"[$sticky_id][$post_id][$slimey_id]",
-			tangible_template('<Loop type="post" sticky="first">[<Field id>]</Loop>')
-		);
-	}
+    // With sticky=false, exclude sticky posts
+    $this->assertEquals(
+      "[$post_id][$post_id_2]",
+      tangible_template('<Loop type=post sticky=false>[<Field id>]</Loop>')
+    );
+
+    // With sticky=only, include sticky posts only
+    $this->assertEquals(
+      "[$sticky_id][$sticky_id_2]",
+      tangible_template('<Loop type=post sticky=only>[<Field id>]</Loop>')
+    );
+  }
 }
