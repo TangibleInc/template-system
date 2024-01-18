fdb18376cfdf37781f4515933fff43ab24553fb6
eliot-akira <me@eliotakira.com>
2024-01-18 17:36:03 +0100

    AJAX: Wrap script in local scope, which was done by build script previously

--- framework/ajax/ajax.js
+++ framework/ajax/ajax.js
@@ -1,47 +1,44 @@
-const { jQuery: $ } = window
+;(function () {
+  const { jQuery: $ } = window
 
-const Tangible = window.Tangible = window.Tangible || {}
-const ajaxConfig = Tangible.ajaxConfig || window.TangibleAjaxConfig || {}
+  const Tangible = (window.Tangible = window.Tangible || {})
+  const ajaxConfig = Tangible.ajaxConfig || window.TangibleAjaxConfig || {}
 
-const {
-  url,
-  nonce
-} = ajaxConfig
-
-function ajax(action, data = {}) {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      type: 'POST',
-      url,
-      data: {
-        action: 'tangible_ajax_' + action,
-        nonce,
-        data
-      },
-      beforeSend: '',
-      success: '',
-      error: ''
-    })
-      .done(function(res) {
-
-        if (res === '1' || res === 1) res = { success: true }
-
-        if (typeof res === 'object' && typeof res.success !== 'undefined') {
-          (res.success ? resolve : reject)(res.data)
-        } else {
-          reject(res)
-        }
+  const { url, nonce } = ajaxConfig
+  function ajax(action, data = {}) {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type: 'POST',
+        url,
+        data: {
+          action: 'tangible_ajax_' + action,
+          nonce,
+          data,
+        },
+        beforeSend: '',
+        success: '',
+        error: '',
       })
-      .fail(function(res) {
-        reject({
-          code: res.status,
-          message: res.statusText
+        .done(function (res) {
+          if (res === '1' || res === 1) res = { success: true }
+
+          if (typeof res === 'object' && typeof res.success !== 'undefined') {
+            ;(res.success ? resolve : reject)(res.data)
+          } else {
+            reject(res)
+          }
         })
-      })
-  })
-}
+        .fail(function (res) {
+          reject({
+            code: res.status,
+            message: res.statusText,
+          })
+        })
+    })
+  }
 
-Tangible.ajax = ajax
+  Tangible.ajax = ajax
 
-// Backward compatibility
-Tangible.ajaxConfig = ajaxConfig
+  // Backward compatibility
+  Tangible.ajaxConfig = ajaxConfig
+})()
