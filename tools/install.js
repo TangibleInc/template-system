4f43a43d6c14ad7d7544e4258ba08839fe55cd57
eliot-akira <me@eliotakira.com>
2024-01-18 17:27:03 +0100

    Local environment: Set debug log true; Preparing install script

--- tools/install.js
+++ tools/install.js
@@ -1,2 +1,7 @@
 
-// for module in editor interface logic template system; do cd $module; npm install --production; done
+/*
+    "https://downloads.wordpress.org/plugin/advanced-custom-fields.latest-stable.zip",
+    "https://downloads.wordpress.org/plugin/beaver-builder-lite-version.latest-stable.zip",
+    "https://downloads.wordpress.org/plugin/elementor.latest-stable.zip",
+    "https://downloads.wordpress.org/plugin/wp-fusion-lite.latest-stable.zip"
+*/
