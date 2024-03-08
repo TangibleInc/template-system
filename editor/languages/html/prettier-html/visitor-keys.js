b2e6c83095e907086295dc82b90c191c26028b3f
eliot-akira <me@eliotakira.com>
2024-03-08 16:48:17 +0100

    Fork prettier/language-html to allow customization such as case-sensitive tag names and raw tags

---
+++ editor/languages/html/prettier-html/visitor-keys.js
@@ -0,0 +1,21 @@
+const visitorKeys = {
+  "front-matter": [],
+  root: ["children"],
+  element: ["attrs", "children"],
+  ieConditionalComment: ["children"],
+  ieConditionalStartComment: [],
+  ieConditionalEndComment: [],
+  interpolation: ["children"],
+  text: ["children"],
+  docType: [],
+  comment: [],
+  attribute: [],
+  cdata: [],
+  angularControlFlowBlock: ["children", "parameters"],
+  angularControlFlowBlockParameters: ["children"],
+  angularControlFlowBlockParameter: [],
+  angularIcuExpression: ["cases"],
+  angularIcuCase: ["expression"],
+};
+
+export default visitorKeys;
