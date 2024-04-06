d2c285d1d53905a60d00766e38793d05426aadd2
eliot-akira <me@eliotakira.com>
2024-04-06 05:08:07 +0200

    Add archive config to generate plugin zip package

--- tangible.config.js
+++ tangible.config.js
@@ -2,4 +2,23 @@ export default {
   format: [
     'tests/**/*.{php,js,jsx,json,ts,tsx,scss}'
   ],
+  archive: {
+    src: [
+      'admin',
+      // 'elandel',
+      'framework',
+      'integrations',
+      'language',
+      'logic',
+      'loop',
+      'modules',
+      'vendor'
+    ],
+    exclude: [
+      'src',
+      'tests',
+    ],
+    dest: 'publish/tangible-template-system.zip',
+    rootFolder: 'tangible-template-system'
+  }
 }
