39e15520aaafc01497d4505f784976b1abaf852d
eliot-akira <me@eliotakira.com>
2024-09-27 19:10:16 +0200

    git subrepo pull (merge) framework
    subrepo:
      subdir:   "framework"
      merged:   "01dc848"
    upstream:
      origin:   "git@github.com:TangibleInc/framework.git"
      branch:   "main"
      commit:   "01dc848"
    git-subrepo:
      version:  "0.4.6"

--- framework/plugin/settings/checkbox.php
+++ framework/plugin/settings/checkbox.php
@@ -16,25 +16,57 @@ namespace tangible\framework;
 function render_setting_field_checkbox($config) {
 
   foreach ([
+    'type',
     'name',
     'value',
-    'label'
+    'label',
+    'description'
   ] as $key) {
-    $$key = $config[$key];
+    $$key = $config[$key] ?? '';
   }
 
   $checked = $value==='true';
 
-  ?>
-  <label>
-    <input type="hidden" name="<?php echo $name; ?>" value="<?php
-      echo $checked ? 'true' : 'false';
-    ?>" autocomplete="off"><input type="checkbox"
-      value="true" autocomplete="off"
-      onclick="this.previousSibling.value=this.previousSibling.value==='true'?'false':'true'"
-      <?php echo $checked ? 'checked="checked"' : ''; ?>
-    />
-    <?php echo esc_html($label); ?>
-  </label>
-  <?php
+  if (empty($type) || $type === 'checkbox'): ?>
+      <label>
+          <input type="hidden" name="<?php echo $name; ?>" value="<?php echo $checked ? 'true' : 'false'; ?>" autocomplete="off">
+          <input type="checkbox" value="true" autocomplete="off"
+              onclick="this.previousSibling.value=this.previousSibling.value==='true'?'false':'true'" 
+              <?php echo $checked ? 'checked' : ''; ?> />
+          <?php echo esc_html($label); ?>
+      </label>
+    <?php elseif ($type === 'switch'): ?>
+        <div class="tangible-card">
+            <label class="tangible-feature-switch" role="switch" aria-checked="<?php echo $checked ? 'true' : 'false'; ?>" tabindex="0" 
+            onclick="this.querySelector('input[type=checkbox]').click();" 
+            onkeydown="if (event.key === ' ' || event.key === 'Enter') { 
+                const checkbox = this.querySelector('input[type=checkbox]') 
+                checkbox.checked = !checkbox.checked
+                checkbox.dispatchEvent(new Event('change'))
+                this.setAttribute('aria-checked', checkbox.checked) 
+            }">
+                <input type="hidden" name="<?php echo $name; ?>" value="<?php echo $checked ? 'true' : 'false'; ?>" autocomplete="off">
+                <input type="checkbox" value="true" autocomplete="off" class="tangible-checkbox" 
+                    onchange="this.previousElementSibling.value = this.checked ? 'true' : 'false'; this.parentElement.setAttribute('aria-checked', this.checked);" 
+                    <?php echo $checked ? 'checked' : ''; ?> style="display: none;" />
+                <span class="tangible-feature-switch-label"><?php echo esc_html($label); ?></span>
+                <span class="tangible-switch-track"></span>
+            </label>
+            <?php if (!empty($description)): ?>
+                <div class="feature-description">
+                    <?php
+                    if (is_callable($description)) {
+                        $description(
+                            tangible\framework\get_plugin_feature_settings($plugin, $feature),
+                            tangible\framework\get_plugin_feature_settings_key($plugin, $feature),
+                            $is_enabled
+                        );
+                    } else {
+                        echo $description;
+                    }
+                    ?>
+                </div>
+            <?php endif; ?>
+      </div>
+    <?php endif; 
 };
