612885cd5b88bf5063279f1206725df3285f4681
eliot-akira <me@eliotakira.com>
2025-08-21 13:35:26 +0200

    Update version

--- logic/module.php
+++ logic/module.php
@@ -3,7 +3,7 @@
 (include __DIR__ . '/module-loader.php')(new class {
 
   public $name = 'tangible_logic';
-  public $version = '20250611';
+  public $version = '20250821';
 
   function load() {
     require_once __DIR__ . '/index.php';
